f4527de8ec39442d71d1fdd492c36f9748e2c93f
Craig Dunn <craig@craigdunn.org>
2023-07-12 12:36:35 +0200

    remove sec

--- instance.tf
+++ instance.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "ec2_instance" {
   instance_type = var.instance_type
   key_name = var.aws_keypair
   count = 5
-  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
 }
 
 
